9cd38a739fc71a91ed492bf00169c617f27366d3
wrickbasu <wrickbasu@gmail.com>
2017-02-28 20:07:14 +0530

    Update 03_logia.py

--- 03_logia.py
+++ 03_logia.py
@@ -20,10 +20,11 @@ from re import search
 
 abort = False
 seterr(divide='ignore', invalid='ignore')    # ignore divide-by-zero error
+path = '/home/wrick/Documents/logia/'
 try:
-    system('nano /home/wrick/Documents/logia/theorem.v')
-    theorem = open('/home/wrick/Documents/logia/theorem.v', 'r').readline()
-    tactics = open('/home/wrick/Documents/logia/tactics_base', 'r').read().split('\n')
+    system('nano ' + path + 'theorem.v')
+    theorem = open(path + 'theorem.v', 'r').readline()
+    tactics = open(path + 'tactics_base', 'r').read().split('\n')
 #    tactics = ['assumption', 'destruct H as [a b]', '']
     T          = len(tactics)        # the last tactic is a null tactic and randint has a closed-open domain
     population = 10                  # constant population size for every generation
@@ -122,7 +123,7 @@ def proofgen(sequence):
 
 # the fitness function
 def fitnesscheck(proof):
-    errfile = open('/home/wrick/Documents/logia/errors', 'r')
+    errfile = open(path + 'errors', 'r')
     totlength = len(proof.strip())
     errlength = int(search('(?<=-)\w+', errfile.read()).group(0))    # search for a word after a hyphen as coqtop gives errors as '... characters xy-zw...'
     fitness = errlength/totlength
@@ -130,167 +131,6 @@ def fitnesscheck(proof):
     return(fitness)
 
 
-# define reproduction -- 2-parent crossover, creates a new proof with new length and all
-def crossover(proof_tuple):
-    (proof1, proof2) = proof_tuple
-    newproof      = []
-    prooflength   = randint(1, maxlength + 1)
-    combine       = proof1 + proof2
-    combinelength = len(combine)
-    for i in range(prooflength):
-        newproof.append(randint(0, combinelength + 1))    # min combinelength = 0
-    return(newproof)
-
-
-# define mutation -- as random as it can be, no dependencies, doesn't change proof length
-def mutagen(proof):
-    mutaprob = 13                         # probability of mutation is 1 in 13, 0 means zero probability
-    newproof = []
-    if mutaprob != 0:
-        for i in proof:
-            if randint(mutaprob) == 0:    # probability of getting a zero is 1/mutaprob
-                newproof.append(randint(T))
-    return(newproof)
-
-
-# normalized inner product between proofs
-def inprod(proof_tuple):
-    (proof1, proof2) = proof_tuple
-    if(len(proof1)>len(proof2)):
-        temp = proof1
-        proof1 = proof2
-        proof2 = temp
-    while(len(proof1)<len(proof2)):
-        proof1.append(0)
-    try:
-        innerproduct = dot(proof1, proof2)/sqrt((dot(proof1, proof1))*(dot(proof2, proof2)))
-    except ZeroDivisionError:
-        innerproduct = 0
-    return(innerproduct)
-
-
-# the new generation
-def generationmap(generation):
-    newgeneration = []
-    for i in range(population):
-        generation[i] = mutagen(generation[i])
-    for i in range(population):
-        proof_tuple = (generation[randint(population)], generation[randint(population)])
-        while inprod(proof_tuple) < proofmatch:
-            proof_tuple = (generation[randint(population)], generation[randint(population)])
-        newproof = crossover(proof_tuple)
-        newgeneration.append(newproof)
-    return(newgeneration)
-
-
-# convert integer sequences to tactic strings
-def proofgen(sequence):
-    #proof = 'Proof '
-    proof = 'Proof. intros.'
-    invalidtactics = ['']    # a list of tactics not to be used in the proof, like one-use tactics, etc
-    for i in sequence:
-        if(tactics[i] not in invalidtactics):
-            proof += ' ' + tactics[i] + '.'
-        invalidtactics.append(tactics[i])
-    proof += ' Defined.\n'
-    return(proof)
-
-
-# the fitness function
-def fitnesscheck(proof):
-    errfile = open('/home/wrick/Documents/logia/errors', 'r')
-    totlength = len(proof.strip())
-    errlength = int(search('(?<=-)\w+', errfile.read()).group(0))    # search for a word after a hyphen as coqtop gives errors as '... characters xy-zw...'
-    fitness = errlength/totlength
-    errfile.close()    
-    return(fitness)
-
-
-# create the seeds -- adds new proofs to the generation
-def seedgen(generation):
-    while len(generation) < population:
-        length = randint(1, maxlength + 1)
-        proof  = []
-        for j in range(length):
-            proof.append(randint(T))
-        generation.append(proof)
-    return(generation)
-
-
-# define reproduction -- 2-parent crossover, creates a new proof with new length and all
-def crossover(proof_tuple):
-    (proof1, proof2) = proof_tuple
-    newproof      = []
-    prooflength   = randint(1, maxlength + 1)
-    combine       = proof1 + proof2
-    combinelength = len(combine)
-    for i in range(prooflength):
-        newproof.append(randint(0, combinelength + 1))    # min combinelength = 0
-    return(newproof)
-
-
-# define mutation -- as random as it can be, no dependencies, doesn't change proof length
-def mutagen(proof):
-    mutaprob = 13                         # probability of mutation is 1 in 13, 0 means zero probability
-    newproof = []
-    if mutaprob != 0:
-        for i in proof:
-            if randint(mutaprob) == 0:    # probability of getting a zero is 1/mutaprob
-                newproof.append(randint(T))
-    return(newproof)
-
-
-# normalized inner product between proofs
-def inprod(proof_tuple):
-    (proof1, proof2) = proof_tuple
-    if(len(proof1)>len(proof2)):
-        temp = proof1
-        proof1 = proof2
-        proof2 = temp
-    while(len(proof1)<len(proof2)):
-        proof1.append(0)
-    try:
-        innerproduct = dot(proof1, proof2)/sqrt((dot(proof1, proof1))*(dot(proof2, proof2)))
-    except ZeroDivisionError:
-        innerproduct = 0
-    return(innerproduct)
-
-
-# the new generation
-def generationmap(generation):
-    newgeneration = []
-    for i in range(population):
-        generation[i] = mutagen(generation[i])
-    for i in range(population):
-        proof_tuple = (generation[randint(population)], generation[randint(population)])
-        while inprod(proof_tuple) < proofmatch:
-            proof_tuple = (generation[randint(population)], generation[randint(population)])
-        newproof = crossover(proof_tuple)
-        newgeneration.append(newproof)
-    return(newgeneration)
-
-
-# convert integer sequences to tactic strings
-def proofgen(sequence):
-    #proof = 'Proof '
-    proof = 'Proof. intros.'
-    invalidtactics = ['']    # a list of tactics not to be used in the proof, like one-use tactics, etc
-    for i in sequence:
-        if(tactics[i] not in invalidtactics):
-            proof += ' ' + tactics[i] + '.'
-        invalidtactics.append(tactics[i])
-    proof += ' Defined.\n'
-    return(proof)
-
-
-# the fitness function
-def fitnesscheck(proof):
-    errfile = open('/home/wrick/Documents/logia/errors', 'r')
-    totlength = len(proof.strip())
-    errlength = int(search('(?<=-)\w+', errfile.read()).group(0))    # search for a word after a hyphen as coqtop gives errors as '... characters xy-zw...'
-    fitness = errlength/totlength
-    errfile.close()    
-    return(fitness)
 
 # the main program
 def logia_main():
